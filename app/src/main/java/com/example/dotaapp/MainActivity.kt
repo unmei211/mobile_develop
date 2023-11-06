4767db258ab5bb7d9452a7bbe3f202f381289fec
vladislav starovoytov <unmei.vs@gmail.com>
2023-11-06 21:06:50 +0600

    начал исправлять. работа будет идти в другой ветке

--- app/src/main/java/com/example/dotaapp/MainActivity.kt
+++ app/src/main/java/com/example/dotaapp/MainActivity.kt
@@ -6,41 +6,29 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -49,13 +37,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.dotaapp.ui.theme.DotaAppTheme
+import com.example.dotaapp.ui.theme.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DotaAppTheme {
-                Surface(modifier = Modifier.fillMaxSize(), color = Color(0xFF050B18)) {
+                Surface(modifier = Modifier.fillMaxSize(), color = appBackgroundColor) {
                     Column(
                         modifier = Modifier.verticalScroll(ScrollState(0))
                     ) {
@@ -74,7 +63,7 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun Header() {
-    Surface(modifier = Modifier.fillMaxSize(), color = Color(0xFF050B18)) {
+    Surface(modifier = Modifier.fillMaxSize(), color = appBackgroundColor) {
         Image(
             painter = painterResource(id = R.drawable.source_dota_head),
             contentDescription = "dota head",
@@ -365,6 +354,7 @@ fun ReviewList() {
             Modifier
                 .padding(top = 24.dp, bottom = 24.dp, start = 13.dp, end = 37.dp)
                 .height(1.dp)
+//                .background(Purple80)
                 .background(Color(0xFF1A1F29))
                 .fillMaxWidth()
         )
